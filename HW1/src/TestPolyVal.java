26b955e824db960386fccd072bec19ab52f46f03
JD <JD@192.168.5.101>
2015-10-22 23:10:42 -0500

    Initial commit

---
+++ HW1/src/TestPolyVal.java
@@ -0,0 +1,21 @@
+/*
+ * file: TestPolyVal.java
+ * author: James Moore
+ * 
+ */
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TestPolyVal {
+	int[] polyArray = new int[10];
+	//ThreadLocalRandom.current().nextInt(-9, 10);
+	//Random rand = 
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
