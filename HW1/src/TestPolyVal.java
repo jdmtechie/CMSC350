525844a89d7bbc5f8b85c285353bca277454c49b
JD Moore <jdmplayer@gmail.com>
2015-10-25 14:18:42 -0500

    Final without test method

--- HW1/src/TestPolyVal.java
+++ HW1/src/TestPolyVal.java
@@ -1,83 +1,75 @@
 /*
  * file: TestPolyVal.java
  * author: James Moore
- * 
  */
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TestPolyVal {
-	static int arrSize = 3;
-	static int x;
-	static int[] polyArray;
+	private int deg;
+	private int x;
+	private int[] polyArray;
 	
-	public static void main(String[] args) {
-		//generate array with random integers -9 thru 9
-		polyArray = new int[arrSize];
+	//Create an Array with randoms -9 to 9 and random x from 1 to 9
+	public TestPolyVal(int arraySize) {
+		polyArray = new int[arraySize];
 		for(int i = 0; i < polyArray.length; i++)
 			polyArray[i] = ThreadLocalRandom.current().nextInt(-9, 9 + 1);
-		
-		//generate random value from 1 to 9 for integer x
+		deg = polyArray.length - 1;
 		x = ThreadLocalRandom.current().nextInt(1, 9 + 1);
-		
-		//print some shit
-		int degree = 0;
-		int coefficient = 0;
-		String polyString = "";
-		for(int i = polyArray.length - 1; i >= 0; i--) {
-			degree = i;
-			coefficient = polyArray[i];
-			if(i == polyArray.length - 1 && coefficient != 0) {
-				polyString += coefficient + "n^" + degree;
-			} else if(i < polyArray.length -1 && coefficient > 0) {
-				polyString += " + " + coefficient + "n^" + degree;
-			} else if(i < polyArray.length -1 && coefficient < 0) {
-				polyString += " - " + (coefficient * -1) + "n^" + degree;
-			} else if(i < polyArray.length -1 && coefficient == 0){
-				polyString += " + " + coefficient;
-			} else {
-				polyString += coefficient;
-			}
-		}
-		
-		System.out.println(polyString);
-		System.out.println("");
-		System.out.println("x = " + x);
-		System.out.println("The coefficients are: " + Arrays.toString(polyArray));
-		System.out.println("");
-		calculateBruteForce(polyArray, x);
-		//System.out.println(equation);
-		
 	}
 	
-	public static void calculateBruteForce(int[] arr, int xVal) {
+	public String toString() {
+		if(deg == 0) return "" + polyArray[0];
+		if(deg == 1 && polyArray[0] > 0 ) return polyArray[1] + "n + " + polyArray[0];
+		if(deg == 1 && polyArray[0] < 0 ) return polyArray[1] + "n - " + (-polyArray[0]);
+		String polyStr = polyArray[deg] + "n^" + deg;
+		for(int i = deg - 1; i >= 0; i--) {
+			if(polyArray[i] >= 0) polyStr += " + " + polyArray[i] + "n^" + i;
+			else if(polyArray[i] < 0) polyStr += " - " + (-polyArray[i]) + "n^" + i;
+			else polyStr += polyArray[i];
+		}
+		return polyStr;
+	}
+	
+	public int calculateBruteForce() {
 		int polyVal = 0;
-		String polyValStr = "";
-	
-		for(int i = 0; i <= arr.length - 1; i++) {
-			if(i == 0) {
-				polyVal += arr[i];
-				polyValStr += " " + arr[i] + " ";
-			} else if(arr[i] == 0) {
-				polyVal += 0;
-				polyValStr += " 0 ";
-			} else {
-				int raised = 0;
-				for(int n = 0; n <= i - 1; n++) {
-					raised += xVal*xVal;
-				} 
-				
-				polyVal += raised;
-				polyValStr += " " + xVal + " ";
+		int raised;
+		for(int i = 0; i <= deg; i++) {
+			if(i == 0) polyVal += polyArray[i];
+			else if(polyArray[i] == 0) polyVal += 0;
+			else {
+				raised = 1;
+				for(int n = 0; n <= i - 1; n++)
+					raised *= x;
+			polyVal += raised * polyArray[i];
 			}
 		}
-		
-		System.out.println(polyVal);
-		System.out.println(polyValStr);
+		return polyVal;
 	}
 	
-	public static void raiseToPower(int[] args) {
-		
+	public int calculateHorner() {
+		int polyVal = 0;
+		for(int i = deg; i >= 0; i--)
+			polyVal = (x * polyVal) + polyArray[i];
+		return polyVal;
 	}
 	
+	public static void main(String[] args) {
+		TestPolyVal a = new TestPolyVal(10);
+		System.out.println(Arrays.toString(a.polyArray));
+		System.out.println(a);
+		System.out.println("n = " + a.x);
+		
+		Long startBrute = System.nanoTime();
+		System.out.println("Brute Force: " + a.calculateBruteForce());
+		Long endBrute = System.nanoTime();
+		
+		Long startHorner = System.nanoTime();
+		System.out.println("Horner: " + a.calculateHorner());
+		Long endHorner = System.nanoTime();
+		
+		System.out.println("Brute: " + (endBrute - startBrute));
+		System.out.println("Horner: " + (endHorner - startHorner));	
+	}
 }
