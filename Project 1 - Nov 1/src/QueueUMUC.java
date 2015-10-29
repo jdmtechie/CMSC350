dc24c8bcef8a3e24e45e03ea2bfd63ebb40a4753
JD Moore <jdmplayer@gmail.com>
2015-10-29 15:21:57 -0500

    Initial Push

---
+++ Project 1 - Nov 1/src/QueueUMUC.java
@@ -0,0 +1,23 @@
+/*
+ * Course: CMSC 350
+ * Assignment: Project 1
+ * Author: James Moore
+ * File: QueueUMUC.java
+ * Date: 1 Nov 2015
+ * IDE: eclipse with JRE 1.8
+ */
+
+
+public class QueueUMUC<T> {
+
+	//Class constructor
+	//public QueueUMUC(int)
+	
+	//void put(T)
+	
+	//T get()
+	
+	//T peek()
+	
+	//public String toString()
+}
---
+++ Project 1 - Nov 1/src/StackUMUC.java
@@ -0,0 +1,26 @@
+/*
+ * Course: CMSC 350
+ * Assignment: Project 1
+ * Author: James Moore
+ * File: StackUMUC.java
+ * Date: 1 Nov 2015
+ * IDE: eclipse with JRE 1.8
+ */
+
+
+public class StackUMUC<T> {
+
+	//Class Constructor
+	//public StackUMUC
+	
+	//void push(T)
+	
+	//T pop()
+	
+	//T peek()
+	
+	//public String toString()
+		
+	
+	
+}
